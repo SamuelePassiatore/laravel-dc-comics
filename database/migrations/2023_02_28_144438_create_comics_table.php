494f1df154b078dedbcb067b5016590dfd03aa25
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-28 16:05:46 +0100

    add elements to function up in migration

--- database/migrations/2023_02_28_144438_create_comics_table.php
+++ database/migrations/2023_02_28_144438_create_comics_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->text('thumb')->nullable();
+            $table->float('price', 4, 2);
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type', 40);
+            $table->text('artists')->nullable();
+            $table->text('writers')->nullable();
             $table->timestamps();
         });
     }
