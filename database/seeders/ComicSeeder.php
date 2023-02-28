9a1a22b3ed3862c5de38b452c36d0f2d7ce7a729
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-28 15:45:51 +0100

    migration and seeder

---
+++ database/seeders/ComicSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
