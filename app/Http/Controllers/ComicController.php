d10e71b53202a47a45ec0bb96d2f3ff925bfc5d9
SamuelePassiatore <semmisno2@protonmail.com>
2023-03-01 14:58:34 +0100

    added file create blade

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -26,6 +26,7 @@ class ComicController extends Controller
      */
     public function create()
     {
+        return view('comics.create');
     }
 
     /**
---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', 'New comics')
+
+@section('content')
+    <h1>Sezione create</h1>
+@endsection
